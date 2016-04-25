52f147d29cb278ce861139024af5fbab4fcc1099
goliatone <burgosemi@gmail.com>
2016-04-25 10:22:15 -0400

    config: Adding gkeypath and gsolver to config module

--- config/index.js
+++ config/index.js
@@ -3,11 +3,20 @@
 var join = require('path').join;
 var resolve = require('path').resolve;
 
-module.exports = {
+var extend = require('gextend');
+var Keypath = require('gkeypath');
+var Solver = require('gsolver');
+var Flattener = require('gsolver/node_modules/flattener');
+
+var solver = new Solver();
+var userConfig = require('./userconfig');
+
+
+
+var data = {
     amqpClient: {
         type: 'amqp',
         json: true,
-        amqp: require('amqp'),
         exchange: process.env.NODE_AMQP_EXCHANGE || ('wework.' + process.env.NODE_ENV),
         client: {
             url: process.env.NODE_AMQP_ENDPOINT
@@ -17,9 +26,38 @@ module.exports = {
         repos: []
     },
     docker: {
-        tagName: 'latest'
+        tagName: 'latest',
+        client: {
+            socketPath: '/tmp/docker.sock'
+        }
     },
-    app:{
-        jobsPath: resolve(join(__dirname, '../jobs'))
+    app: {
+        jobsPath: resolve(join(__dirname, '../jobs')),
+        url: 'http://localhost:3030',
+        storage: 'file'
+    },
+    builds: {
+        concurrent: 1,
+        'retry-after': 30
+    },
+    paths: {
+        builds: '${app.url}/tmp/rabbithook-builds',
+        sources: '${paths.builds}/sources',
+        tars: '${paths.builds}/tars',
+        logs: '${paths.builds}/logs'
     }
 };
+
+data = extend({}, data, userConfig);
+
+data = solver.solve(data);
+
+var config = Keypath.wrap(data);
+
+/*
+ * We need to use require after solving
+ * else it flattener breaks :P
+ */
+config.amqp = require('amqp');
+
+module.exports = config;
